fed82756d3e36e9bfb1e6acbf4dd2ee11550864b
kovidjuneja <127709361+kovidjuneja@users.noreply.github.com>
2024-07-22 16:49:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 os.environ['LD_LIBRARY_PATH'] = '/path/to/libGL.so.1'
 import streamlit as st
 import urllib
-import tensorflow as tf
 import cv2
 import numpy as np
 from PIL import Image
